bf304f597af8952575009e638cf8b4357da6873f
unknown <marsandnoa@gmail.com>
2024-07-31 19:42:19 -0400

    added hook generation, switched to flask, added batch scripts for starting closing site

--- Backend/api/src/main/java/com/api/api/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api.api.repositories;
-
-import com.api.api.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
